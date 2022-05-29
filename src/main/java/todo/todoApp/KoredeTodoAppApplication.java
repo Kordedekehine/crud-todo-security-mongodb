4c4aa57c7b7f9a0de129e1f08ed7d10a843882de
amigoscode <salamikehinde345@gmail.com>
2022-05-29 08:06:24 +0100

    the db now connected

--- src/main/java/todo/todoApp/KoredeTodoAppApplication.java
+++ src/main/java/todo/todoApp/KoredeTodoAppApplication.java
@@ -1,13 +1,16 @@
 package todo.todoApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class KoredeTodoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KoredeTodoAppApplication.class, args);
 	}
+
 
 }
