9134013410dd4e053a0389d0fd352b52ec38ddd1
amigoscode <salamikehinde345@gmail.com>
2022-05-31 11:45:54 +0100

    test

--- src/main/java/todo/todoApp/KoredeTodoAppApplication.java
+++ src/main/java/todo/todoApp/KoredeTodoAppApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//(exclude = {DataSourceAutoConfiguration.class})
+//@EnableMongoRepositories
+@SpringBootApplication
 public class KoredeTodoAppApplication {
 
 	public static void main(String[] args) {
--- src/test/java/todo/todoApp/todoRepositoryTests.java
+++ src/test/java/todo/todoApp/todoRepositoryTests.java
@@ -1,22 +1,44 @@
 package todo.todoApp;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 import todo.todoApp.model.Todo;
 import todo.todoApp.repository.TodoRepository;
 
+import java.sql.Date;
+
+//@RunWith(SpringRunner.class)
 @DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class todoRepositoryTests {
 
     @Autowired
     TodoRepository todoRepository;
 
+    @Autowired
+    TestEntityManager testEntityManager;
+
     @Test
     public void saveTodoTest(){
-        Todo todos = new Todo(){
+     Todo todo = new Todo();
+     todo.setUserName("korede");
+     todo.setDescription("i wanna go to the gym");
+     todo.setId(1L);
+     todo.setTargetDate(Date.valueOf("12/02/2023"));
 
+     todoRepository.save(todo);
+     Assertions.assertThat(todo.getId()).isGreaterThan(0);
+        }
 
+        public void getTodoTest(){
+        Todo todo = todoRepository.findById(1L).get();
+        Assertions.assertThat(todo.getId()).isEqualTo(1L);
         }
     }
-}
+
